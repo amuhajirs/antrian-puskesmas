d92bfe242cf6b2ba419f9eeaa358d0c26ac662e5
althafi <altp.hlnshr14@gmail.com>
2023-02-07 11:42:31 +0700

    antrian

--- resources/views/partials/ambil-antrian.blade.php
+++ resources/views/partials/ambil-antrian.blade.php
@@ -20,29 +20,31 @@
                 <div class="row justify-content-md-center">
                     <div class="col-md-12" style="margin-top: 20px">
                         <!-- <h1 align="center">Login </h1> -->
-                        <form action="/Index/saveAntrian" method="post">
-                            <div class ="row">
-                                <div class ="col-md-2">
+                        <form action="/Index/saveAntrian" method="POST">
+                            <div class="row mb-2">
+                                <div class ="col-lg-2">
                                     <h6><label>Pilih Poli</label></h6>   
                                 </div>
 
-                                <div class="col-md-5">
+                                <div class="col-lg-5">
                                     <select name="id_poli" id="id_poli" class="form-control" onchange="noAntrian(this.value)">
-                                        <option value=""> pilih </option>
-                                        <option value="1"> PLUM </option>
-                                        <option value="2"> PLGG </option>
-                                        <option value="3"> PLIM </option>
-                                        <option value="4"> PLTB </option>
+                                        <option value="">Pilih</option>
+                                        <option value="poli-mata">Poli Mata</option>
+                                        <option value="poli-anak">Poli Anak</option>
+                                        <option value="poli-tht">Poli THT</option>
+                                        <option value="poli-jiwa">Poli Jiwa</option>
+                                        <option value="poli-saraf">Poli Saraf</option>
+                                        <option value="poli-gizi">Poli Gizi</option>
                                     </select>
                                 </div>
                             </div>
 
-                            <div class ="row">
-                                <div class ="col-md-2">
+                            <div class ="row mb-2">
+                                <div class ="col-lg-2">
                                     <h6><label>No Antrian Poli</label></h6>    
                                 </div>
 
-                                <div class="col-md-5">
+                                <div class="col-lg-5">
                                     <input type="text" name="no_antrian_poli2" id="no_antrian_poli2" value="" disabled="" class="form-control">
                                     <input type="hidden" name="no_antrian_poli" id="no_antrian_poli" value="" class="form-control">
                                     <input type="hidden" name="no_antrian" value="5">
@@ -51,20 +53,24 @@
 
                             
                             <div class="row text-right">
-                                <div class="col-md-7">
+                                <div class="col-lg-7">
                                     <input type="submit" name="simpan" id="simpan" value="Ambil Antrian" class="btn btn-primary">
                                 </div>
                             </div>
                         </form>
 
                         <div class="row text-justify" >
-                            <h6>Keterangan :</h6>
-                            <ol>
-                                <li>Poli Umum (PLUM)</li>
-                                <li>Poli Gigi (PLGG) </li>
-                                <li>Poli Imunisasi (PLIM)</li>
-                                <li>Poli Tuberculosis (PLTB)</li>
-                            </ol>
+                            <div class="col-lg-2">
+                                <h6>Keterangan :</h6>
+                            </div>
+                            <div class="col-lg-5">
+                                <ol class="pl-3">
+                                    <li>Poli Umum (PLUM)</li>
+                                    <li>Poli Gigi (PLGG) </li>
+                                    <li>Poli Imunisasi (PLIM)</li>
+                                    <li>Poli Tuberculosis (PLTB)</li>
+                                </ol>
+                            </div>
                         </div>
                     </div>
 
